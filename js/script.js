ffcdaffb3d445483d63c4ba25f48623321c7b7cb
Devysson Cardoso <devyssonsc@gmail.com>
2023-11-29 23:05:57 +0000

    process error resolved

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const apiKey = "95efa740d2c65087ee8a19dfd32984c0";
+
 const formSearchCity = document.querySelector("#searchCity")
 const nameCity = document.querySelector("#nameCity");
 const temp = document.querySelector("#temp");
@@ -10,31 +12,28 @@ const visibility = document.querySelector("#visibility");
 const informations = document.querySelector("#informations");
 const h1 = document.querySelector("h1");
 
-const SearchCity = (city) => {
-    const result = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=95efa740d2c65087ee8a19dfd32984c0&lang=pt`)
+const SearchCity = async (city) => {
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt`)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data);
         return data;
     });
-    console.log(result)
     return result
 };
 
-formSearchCity.addEventListener("submit", (e) => {
+formSearchCity.addEventListener("submit", async (e) => {
     e.preventDefault();
     const city = formSearchCity.inputCity.value;
-    SearchCity(city).then((data) => {
-        console.log(data);
-        nameCity.innerText = data.name;
-        temp.innerText = Number(data.main.temp).toFixed(0) + "°";
-        condition.innerText = data.weather[0].main;
-        maxTemp.innerText = Number(data.main.temp_max).toFixed(0) + "°";
-        minTemp.innerText = Number(data.main.temp_min).toFixed(0) + "°";
-        windSpeed.innerText = Number(data.wind.speed).toFixed(0);
-        humidity.innerText = data.main.humidity;
-        visibility.innerText = (Number(data.visibility) / 1000).toFixed(0);
-    });
+    const result = await SearchCity(city);
+    console.log(result);
+    nameCity.innerText = result.name;
+    temp.innerText = Number(result.main.temp).toFixed(0) + "°";
+    condition.innerText = result.weather[0].main;
+    maxTemp.innerText = Number(result.main.temp_max).toFixed(0) + "°";
+    minTemp.innerText = Number(result.main.temp_min).toFixed(0) + "°";
+    windSpeed.innerText = Number(result.wind.speed).toFixed(0);
+    humidity.innerText = result.main.humidity;
+    visibility.innerText = (Number(result.visibility) / 1000).toFixed(0);
 
     informations.classList.remove("hidden");
     h1.classList.add("hidden");
